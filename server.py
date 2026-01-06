549b8b2fb865e8248324241e727f3355cf54dabe
Layesh Padole <padolelayesh@gmail.com>
2026-01-06 15:30:32 +0530

    fix: add toolkit

---
+++ server.py
@@ -0,0 +1,35 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+# Create an MCP server
+mcp = FastMCP(
+    name="Calculator",
+    host="0.0.0.0",  # only used for SSE transport (localhost)
+    port=8050,  # only used for SSE transport (set this to any port)
+)
+
+
+# Add a simple calculator tool
+# adding a simple tool here, you can add more tools as needed
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Run the server
+if __name__ == "__main__":
+    transport = os.getenv("TRANSPORT", "stdio")
+    if transport == "stdio":
+        mcp.run(transport="stdio")
+    elif transport == "sse":
+        print("Running server with SSE transport")
+        mcp.run(transport="sse")
+    elif transport == "streamable-http":
+        print("Running server with Streamable HTTP transport")
+        mcp.run(transport="streamable-http")
+    else:
+        raise ValueError(f"Unknown transport: {transport}")
